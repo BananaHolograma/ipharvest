d95024c1f7b5e633662d647ba0b2243c08d89119
0xp1n <s3r0s4pi3ns@protonmail.com>
2023-04-10 15:06:09 +0100

    removed bin and build folders

--- build/compile.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-CURRENT_DIR=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
-
-shc -f "$CURRENT_DIR/../ipharvest.sh" -o "$CURRENT_DIR/../bin/ipharvest"
-
-rm "$CURRENT_DIR/../ipharvest.sh.x.c" 2>/dev/null
